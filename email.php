a011ff6e10e9bcee06e47e76fd2dd54c63dbf217
junejuly-jb <58539877+junejuly-jb@users.noreply.github.com>
2020-04-28 18:06:17 +0800

    added SWAL

--- email.php
+++ email.php
@@ -5,7 +5,7 @@ require("PHPMailer-master/src/PHPMailer.php");
 require("PHPMailer-master/src/SMTP.php");
 require("PHPMailer-master/src/Exception.php");
 
-if(isset($_POST['name']) && isset($_POST['email'])){
+if(isset($_POST) && isset($_POST['email'])){
 
 $email = $_POST['email'];
 $company = $_POST['company'];
@@ -16,7 +16,7 @@ $mail = new PHPMailer\PHPMailer\PHPMailer();
 
 $mail->IsSMTP(); 
 
-$mail->CharSet="UTF-8";
+$mail->CharSet="UTF-8";  
 $mail->Host = "smtp.gmail.com";
 $mail->SMTPDebug = 1; 
 $mail->Port = 587 ; //465 or 587
@@ -26,30 +26,20 @@ $mail->SMTPAuth = true;
 $mail->IsHTML(true);
 
 //Authentication
-$mail->Username = "matias@bpoc.co.jp";
-$mail->Password = "matt1994";
+$mail->Username = "junearagoncillo@bpoc.co.jp";
+$mail->Password = "junearagonszx111";
 
 //Set Params
 $mail->SetFrom($email,$name);
-$mail->AddAddress("matias@bpoc.co.jp");
+$mail->AddAddress("junearagoncillo@bpoc.co.jp");
 $mail->Subject = "Inquiries";
 $mail->Body ="<h2><b>Name: ".$name."<br>Company ".$company."<br>Email ".$email."<br>Number: ".$number."<br>Message: ".$msg."</b></h2>";
 
     if(!$mail->Send()) {
-        echo "Mailer Error";
-        // echo "Mailer Error: " . $mail->ErrorInfo;
-      $msg = "Email not sent";
+        echo "email not sent";
         
     } else {
-     //echo "Message has been sent";
-    // echo "<script>window.open('index.html','_self')</script>";
-    $signal = 'ok';
-    $msg = 'Email sent';
-    }
-    $data = array(
-        'signal' => $signal,
-        'msg' => $msg
-    );
-    echo json_encode($data);
+     echo "Message has been sent";
+}
 }
 ?>		
